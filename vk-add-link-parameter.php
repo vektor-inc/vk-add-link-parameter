97ab56f29ba1dde834eeb9f4df0928baf47b1db5
drill-lancer <rickaddison7634@gmail.com>
2025-07-25 10:12:30 +0900

    fix: deploy

--- vk-add-link-parameter.php
+++ vk-add-link-parameter.php
@@ -5,25 +5,24 @@
  * Version: 1.0
  * Author: Vektor, Inc.
  * Author URI: https://www.vektor-inc.co.jp
+ *
+ * @package VK Add Link Parameter
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
+	exit;
 }
 
 /**
- * Adds a custom parameter to the VK share link.
- *
- * @param string $url The original URL.
- * @return string Modified URL with the custom parameter.
+ * Add Link Script
  */
 function valp_add_script() {
-    wp_enqueue_script(
-        'vk-add-link-parameter',
-        plugin_dir_url( __FILE__ ) . 'build/vk-add-link-parameter.js',
-        array(),
-        '1.0',
-        true
-    );
+	wp_enqueue_script(
+		'vk-add-link-parameter',
+		plugin_dir_url( __FILE__ ) . 'build/vk-add-link-parameter.js',
+		array(),
+		'1.0',
+		true
+	);
 }
 add_action( 'wp_enqueue_scripts', 'valp_add_script' );
